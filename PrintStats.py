004c80e6ffceb8e0d860a78af998a1a423aab740
Nickola Trupcheff <n.trupcheff@gmail.com>
2025-10-22 10:11:02 +0300

    Stats Page improvements

--- PrintStats.py
+++ PrintStats.py
@@ -2,9 +2,12 @@ import time
 import sys
 
 class PrintStats:
-    column_1_width: int = 45
-    column_2_width: int = 15
-    column_3_width: int = 17
+    column_1_width: int = 30
+    column_2_width: int = 10
+    column_3_width: int = 15
+    column_4_width: int = 17
+    column_5_width: int = 17
+    column_6_width: int = 7
 
     @staticmethod
     def print_red(message, end = '\n'):
@@ -19,42 +22,67 @@ class PrintStats:
         sys.stderr.write('\x1b[1;33m' + message + '\x1b[0m' + end)
 
     @staticmethod
-    def print_stats(config, banned_ips, ip_stats, status_by_ip_stats, referer_stats, times_by_url_stats, ua_stats) -> None:
+    def print_stats(config, banned_ips, ip_stats, times_by_url_stats, ua_stats) -> None:
         current_time = time.time()
         print("\033c", end="")  # Clear screen
         print(f"Running: {time.time() - config['start_time']:.2f} seconds  |  Parsed: {config['lines_parsed']} lines  |  Time frame: {config['time_frame']} seconds")
-        print(f"{'IP Address':<{PrintStats.column_1_width}} {'Upstream Time':<{PrintStats.column_2_width}} {'Requests':<{PrintStats.column_3_width}}")
-        print("=" * 100)
+        print(
+            f"{'IP Address':^{PrintStats.column_1_width}}|"
+            f"{'Requests':^{PrintStats.column_2_width}}|"
+            f"{'Upstream Time':^{PrintStats.column_3_width}}|"
+            f"{'Bad HTTP Status':^{PrintStats.column_4_width}}|"
+            f"{'HTTP Referes':^{PrintStats.column_5_width}}|"
+            f"{'Score':^{PrintStats.column_6_width}}|"
+        )
+        print(
+            f"{'':^{PrintStats.column_1_width}}|"
+            f"{'':^{PrintStats.column_2_width}}|"
+            f"{'':^{PrintStats.column_3_width}}|"
+            f"{'':^{PrintStats.column_4_width}}|"
+            f"{' unknown    none':<{PrintStats.column_5_width}}|"
+            f"{'':^{PrintStats.column_6_width}}|"
+        )
+        print("=" * 127)
 
         # sort by total_time
         counter = 0
-        for ip, stats in sorted(ip_stats.items(), key=lambda item: item[1]['total_time'], reverse=True):
+        for ip, stats in sorted(ip_stats.items(), key=lambda item: (item[1]['score'], item[1]['request_count']), reverse=True):
             counter += 1
             if counter > config['detail_lines']:
                 break
-            message = f"{ip:<{PrintStats.column_1_width}.{PrintStats.column_1_width}} {stats['total_time']:>{PrintStats.column_2_width}.3f}/{stats['avail_time']:<15.3f} {stats['request_count']:5d}rq"
+            upstream_stats = f"{stats['total_time']:.2f}/{int(stats['avail_time'])}"
+            status_stats = f"{stats['http_status']['bad']} ({stats['http_status']['bad_perc']:.2f}%)"
+            referer_stats = f"{stats['referer']['unrelated']:6d} {stats['referer']['no_referer']:6d} "
+            message = (f"{ip:<{PrintStats.column_1_width}.{PrintStats.column_1_width}}|"
+                    f"{stats['request_count']:>{PrintStats.column_2_width}d}|"
+                    f"{upstream_stats:>{PrintStats.column_3_width}.{PrintStats.column_3_width}}|"
+                    f"{status_stats:>{PrintStats.column_4_width}.{PrintStats.column_4_width}}|"
+                    f"{referer_stats:>{PrintStats.column_5_width}.{PrintStats.column_5_width}}|"
+                    f"{stats['score']:>{PrintStats.column_6_width}.2f}|"
+            )
             if ip in banned_ips:
                 PrintStats.print_red(message)
-            elif stats['total_time'] > stats['avail_time'] * 0.5 and stats['request_count'] > 10:
+            elif stats['score'] >= 1:
                 PrintStats.print_yellow(message)
             else:
                 print(message)
 
         print()
         counter = 0
-        for ip, data in sorted(status_by_ip_stats.items(), key=lambda item: (item[1]['bad_perc'], item[1]['count']), reverse=True):
-            if counter > config['detail_lines']:
-                break
-            counter += 1
-            message = f"{ip:<{PrintStats.column_1_width}.{PrintStats.column_1_width}} All: {data['count']:>10}, Bad http_status: {data['bad_perc']:>8.2f}%"
-            if ip in banned_ips:
-                PrintStats.print_red(message)
-            elif data['bad_perc'] > 10.0:
-                PrintStats.print_yellow(message)
-            else:
-                print(message)
+        if False:
+            for ip, data in sorted(ip_stats.items(), key=lambda item: (item[1]['http_status']['bad_perc'], item[1]['http_status']['count']), reverse=True):
+                if counter > config['detail_lines']:
+                    break
+                counter += 1
+                message = f"{ip:<{PrintStats.column_1_width}.{PrintStats.column_1_width}} All: {data['http_status']['count']:>10}, Bad http_status: {data['http_status']['bad_perc']:>8.2f}%"
+                if ip in banned_ips:
+                    PrintStats.print_red(message)
+                elif data['http_status']['bad_perc'] > 10.0:
+                    PrintStats.print_yellow(message)
+                else:
+                    print(message)
 
-        if config['referer_stats']:
+        if False and config['referer_stats']:
             print()
             counter = 0
             for ip, stats in sorted(referer_stats.items(), key=lambda item: int(item[1]['unrelated']) + int(item[1]['no_referer']), reverse=True):
