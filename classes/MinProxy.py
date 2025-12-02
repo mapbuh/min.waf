7352db863732d05ee7d7eeb8fe735fda9424fbb8
Nickola Trupcheff <n.trupcheff@gmail.com>
2025-12-02 15:53:38 +0200

    Done chasing empty requests

--- classes/MinProxy.py
+++ classes/MinProxy.py
@@ -82,17 +82,12 @@ class MinProxy:
                 # eof
                 break
             buffer += data
-            if (buffer.find(b'\r\n\r\n') != -1 or buffer.find(b'\n\n') != -1) and len(buffer) > 100:
+            if (buffer.find(b'\r\n\r\n') != -1 or buffer.find(b'\n\n') != -1) and len(buffer) > 1:
                 break
         buffer_decoded = buffer.decode(errors='ignore')
         host_match = re.search(r'^Host: (.*)$', buffer_decoded, re.MULTILINE)
         if host_match:
             log_line_data['host'] = host_match.group(1).strip().split(':')[0]
-        if not log_line_data['host'] or str(log_line_data['host']).strip() == '':
-            logging.warning(f"No Host header in request from {addr}")
-            logging.info(f"line: {buffer_decoded}")
-            request_socket.close()
-            return
         ip_match = re.search(r'^X-Real-IP: (.*)$', buffer_decoded, re.MULTILINE)
         if ip_match:
             log_line_data['ip'] = ip_match.group(1).strip()
@@ -133,7 +128,7 @@ class MinProxy:
                 log_line_data['upstream_response_time'] = time.time() - float(log_line_data['req_ts'])
                 log_line = LogLine(log_line_data)
                 log_line_data['logged'] = True
-                Nginx.process_line(self.config, self.rts, log_line, "", debug=buffer_decoded)
+                Nginx.process_line(self.config, self.rts, log_line, "")
                 request_socket.close()
                 response_socket.close()
                 return
@@ -141,7 +136,7 @@ class MinProxy:
             log_line_data['upstream_response_time'] = time.time() - float(log_line_data['req_ts'])
             log_line = LogLine(log_line_data)
             log_line_data['logged'] = True
-            Nginx.process_line(self.config, self.rts, log_line, "", debug=buffer_decoded)
+            Nginx.process_line(self.config, self.rts, log_line, "")
             return
         header_end = False
         http_status: str = '200'
@@ -174,15 +169,7 @@ class MinProxy:
                                 log_line_data['upstream_response_time'] = time.time() - float(log_line_data['req_ts'])
                                 log_line = LogLine(log_line_data)
                                 log_line_data['logged'] = True
-                                if (
-                                    Nginx.process_line(
-                                        self.config,
-                                        self.rts,
-                                        log_line,
-                                        "",
-                                        debug=buffer_decoded
-                                    ) == Nginx.STATUS_BANNED
-                                ):
+                                if (Nginx.process_line(self.config, self.rts, log_line, "") == Nginx.STATUS_BANNED):
                                     # just enough for iptables to register the ban and annoy the attacker a bit
                                     time.sleep(3)
                                     # and confuse them, in case it hasn't propagated yet
@@ -206,5 +193,5 @@ class MinProxy:
         if not log_line_data['logged']:
             log_line_data['upstream_response_time'] = time.time() - float(log_line_data['req_ts'])
             log_line = LogLine(log_line_data)
-            Nginx.process_line(self.config, self.rts, log_line, "", debug=buffer_decoded)
+            Nginx.process_line(self.config, self.rts, log_line, "")
         return
--- classes/Nginx.py
+++ classes/Nginx.py
@@ -136,14 +136,14 @@ class Nginx:
         return path
 
     @staticmethod
-    def process_line(config: Config, rts: RunTimeStats, log_line: LogLine, line: str, debug: str = "") -> str:
+    def process_line(config: Config, rts: RunTimeStats, log_line: LogLine, line: str) -> str:
         ua_data: IpData | None = None
         url_data: IpData | None = None
 
+        if log_line.ip.strip() == '' and log_line.host.strip() == '':
+            logging.debug(f"empty request: {line}")
+            return Nginx.STATUS_UNKNOWN
         rts.lines_parsed += 1
-        if log_line.ip.strip() == '':
-            logging.info(f"strange ip {log_line.host=} {log_line.ip=}")
-            logging.info(f"line: {debug}")
         if rts.ip_whitelist.is_whitelisted(log_line.host, log_line.ip):
             return Nginx.STATUS_OK
         if Bots.good_bot(config, log_line.ua):
